ecafc0fdd4901f4f370c4e5108640bac9b526471
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-18 13:52:28 -0500

    created html Routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const router = require("express").Router();
+const uniqid = require("uniqid");
+// const db = JSON.parse(fs.readFile(""));
+
+// // router.get("/notes", (req, res) => {
+// //   res.send(db);
+// // });
+
+module.exports = router;
--- routes/apiRoutes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { notes } = require("../../db/db.json");
-
-router.get("/notes", (req, res) => {
-  res.json("Do You see Me? " + notes);
-});
-
-module.exports = router;
