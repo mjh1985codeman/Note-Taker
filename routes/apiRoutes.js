81a1f63fac38a5763dc9f5917b25ab310be6d343
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-18 14:02:20 -0500

    got my notes api route working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const router = require("express").Router();
 const uniqid = require("uniqid");
-// const db = JSON.parse(fs.readFile(""));
+const db = JSON.parse(fs.readFileSync("./db/db.json"));
 
-// // router.get("/notes", (req, res) => {
-// //   res.send(db);
-// // });
+router.get("/notes", (req, res) => {
+  res.send(db);
+});
 
 module.exports = router;
