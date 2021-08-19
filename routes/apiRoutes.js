3afa575d4cf6597a8873ddad99a16fa7b5a65cf3
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-19 17:22:48 -0500

    began work w/ uniqid. . .trying to push a unique id w/ each note saved. . .WIP

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,29 @@
 const fs = require("fs");
 const router = require("express").Router();
-// const { handleNoteSave } = require("../public/assets/js/index");
+const { notes } = require("../db/db.json");
 const uniqid = require("uniqid");
+// created db variable as the db.json file
 const db = JSON.parse(fs.readFileSync("./db/db.json"));
-//get route to pull the existing notes from the db.json file.
+
+let noteId = uniqid();
+
+console.log(noteId);
+
+// Setup /notes Routes
 router.get("/notes", (req, res) => {
   res.send(db);
 });
+
 // router.post work.
 router.post("/notes", (req, res) => {
   res.json(db);
   //pushes the new note to the db.json file.
-  db.push({ title: req.body.title, text: req.body.text });
+  db.push({
+    title: req.body.title,
+    text: req.body.text,
+    //Why is my note ID not pushing to the db???
+    noteId: req.body.noteId,
+  });
   //writes the newly updated db.json file to the html.
   fs.writeFileSync("./db/db.json", JSON.stringify(db));
 });
